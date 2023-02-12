8563abead7759107e6afa0f3abf763af7cd264f3
MOCHAZO <123989587+MOCHAZO@users.noreply.github.com>
2023-02-12 19:20:45 +0100

    Add files via upload

---
+++ player_combat.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_combat : MonoBehaviour
+{
+    // VARIABLES
+    // Projectile list
+    public List<GameObject> projectile_list = new List<GameObject>();
+
+    // Import player GameObject
+    public GameObject player;
+
+    // Import Firepoint
+    public GameObject firepoint;
+
+    // Attack Cooldown, minimu is set in powerup_attack
+    public float attack_cd;
+
+    // Clock
+    private float clock;
+
+    //PRUEBA
+    // Import laser sound
+    public AudioSource laser_sound;
+    public AudioSource enemy_death;
+
+
+    // Start is called at the start
+    private void Start()
+    {
+        // Set Variables
+        attack_cd = 1f;
+
+        //PRUEBA
+        // Volume of the laser sound
+        laser_sound.volume = 0.20f;
+        enemy_death.volume = 0.20f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increase clock time
+        clock += Time.deltaTime;
+
+        // Attack automatically
+        if (attack_cd <= clock)
+        {
+            search_Projectil();
+        }
+    }
+
+
+    // FUNCTIONS
+    public void search_Projectil()
+    {
+        // Check bullet list for innactive bullets
+        foreach (GameObject item in projectile_list)
+        {
+            // Play laser sound when shooting
+            laser_sound.Play();
+            if (item.gameObject.activeInHierarchy == false)
+            {
+                // Activate an innactive bullet
+                item.gameObject.SetActive(true);
+
+                // Move the bullet to the player's firepoint
+                item.transform.position = firepoint.transform.position;
+
+                // Rotate the projectile to face the same direction than the player's firepoint
+                item.transform.rotation = firepoint.transform.rotation;
+
+                // Reset Clock
+                clock = 0f;
+                return;
+            }
+        }
+        // If no bullets were found
+        Debug.Log("No stored bullets available");
+    }
+
+    //PRUEBA
+    public void playEnemyDeathSound()
+    {
+        enemy_death.Play();
+    }
+}
---
+++ player_hp.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_hp : MonoBehaviour
+{
+    // VARIABLES
+    // Player Hp
+    public float hp;
+
+    // Boolean that checks true when the player is colliding
+    private bool colliding;
+
+    // Player Invulnerability time after taking dmg
+    public float inv_time;
+
+    // Display player's hp
+    public Text hp_text;
+
+    // Clock
+    private float clock;
+
+    // PRUEBA
+    // Death sound
+    public AudioSource death_sound;
+    // Hit sound
+    public AudioSource hit_sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set variables
+        hp = 3f;
+        inv_time = 1f;
+        colliding = false;
+
+        //PRUEBA
+        death_sound.volume = 0.1f;
+        hit_sound.volume = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increase clock
+        clock += Time.deltaTime;
+
+        // Player takes constant damage if it's colliding with something
+        if (inv_time < clock && colliding == true)
+        {
+            hit_sound.Play();
+            hp -= 1;
+            // Updates HP display
+            hp_text.text = $"{hp}";
+            
+            // If player reaches 0 hp, trigger GameOver screen
+            if (hp <= 0)
+            {
+                //SceneManager.LoadScene("Lose");
+
+                // PRUEBA
+                death_sound.Play();
+                Debug.Log("Player Loses");
+                
+            }
+            
+            // Reset the clock
+            clock = 0f;
+        }
+    }
+
+    // COLLISIONS
+    // Turn colliding to true when player collides
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag != "Power")
+        {
+            colliding = true;
+            Debug.Log("true");
+        }
+    }
+
+    // Turn colliding to false when player collides
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Power")
+        {
+            colliding = false;
+            Debug.Log("false");
+        }
+    }
+}
